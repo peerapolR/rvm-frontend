8ac29f8e3dd7e9d70f00ba9a93a36b2f4eaf9c05
kvinbindai <kavinsanguansook@gmail.com>
2024-12-05 22:51:46 +0700

    fix layout and footer in bottom page

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -6,7 +6,7 @@ import { Content, Header } from "antd/es/layout/layout";
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="p-24">
       {/* <div>Main Page</div> */}
     </main>
   );
